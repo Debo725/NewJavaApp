75fe4f2b12ea41e6d8bf104cbe1db0abe32e0b0c
ADEBOWALE <116038673+Debo725@users.noreply.github.com>
2022-12-07 11:16:07 -0500

    Update App.java

--- WebApp/src/main/java/WebApp/App.java
+++ WebApp/src/main/java/WebApp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "GitHub Webhook!" );
+        System.out.println( "GitHub Webhook Reviewed!" );
     }
 }
